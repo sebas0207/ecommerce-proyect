30f6d877b37bf2669c2cc08768247ab58c540466
sebas <sebastianmq027@gmail.com>
2025-03-21 13:57:31 +0100

    Add products data to store with details and images

---
+++ src/store/products.ts
@@ -0,0 +1,149 @@
+import type { Product } from "../types";
+
+export const products: Product[] = [
+    {
+        id : "1",
+        name: "Nike-cortez",
+        price: 190.99,
+        imageUrl : "/imagesProducts/nike-cortez.webp",  
+    },
+    {
+        id : "2",
+        name: "Nike-700",
+        price: 170.99,
+        imageUrl : "/imagesProducts/nike-700.webp",   
+    },
+    {
+        id: "3",
+        name:"Nike-dunk-low",
+        price : 199.99,
+        imageUrl:"/imagesProducts/nike-dunk-low.webp",
+    },
+    {
+        id: "4",
+        name:"Nike-genesis",
+        price : 199.99,
+        imageUrl:"/imagesProducts/nike-genesis.webp",
+    },
+    {
+        id: "5",
+        name:"Adidas-yeezy",
+        price : 219.99,
+        imageUrl:"/imagesProducts/adidas-yeezy.webp",
+    },
+    {
+        id: "6",
+        name:"Adidas-yeezy-black",
+        price : 219.99,
+        imageUrl:"/imagesProducts/adidas-yeezy-black.webp",
+    },
+    {
+        id: "7",
+        name:"AdidasXghoster",
+        price : 219.99,
+        imageUrl:"/imagesProducts/adidasXghoster.webp",
+    },
+    {
+        id: "8",
+        name:"Nike-abundance",
+        price : 222.99,
+        imageUrl:"/imagesProducts/nike-abundance.webp",
+    },
+    {
+        id: "9",
+        name:"Nike-air-max-1",
+        price : 119.99,
+        imageUrl:"/imagesProducts/nike-air-max-1.webp",
+    },
+    {
+        id: "10",
+        name:"Vomero-nike",
+        price : 120.99,
+        imageUrl:"/imagesProducts/vomero-nike.webp",
+    },
+    {
+        id: "11",
+        name: "Adidas-superstar-negro",
+        price: 140.99,
+        imageUrl: "/imagesProducts/adidas-superstar-negro.webp"
+    },
+    {
+        id: "12",
+        name: "Jordan-heir-sail",
+        price: 300.00,
+        imageUrl: "/imagesProducts/jordan-heir-sail.webp",
+    },
+    {
+        id: "13",
+        name: "Adidas-evo",
+        price: 129.99,
+        imageUrl: "/imagesProducts/adidas-evo.webp",
+    },
+    {
+        id: "14",
+        name: "Air-jordan-zion-4",
+        price: 140.00,
+        imageUrl: "/imagesProducts/jordan-zion-4.webp", 
+    },
+    {
+        id: "15",
+        name: "Nike-concepts",
+        price: 150.00,
+        imageUrl: "/imagesProducts/nike-concepts.webp", 
+    },
+    {
+        id: "16",
+        name: "Nike-shox-black",
+        price: 129.00,
+        imageUrl: "/imagesProducts/nike-shox-black.webp", 
+    },
+    {
+        id: "17",
+        name: "Nike-tn-blue",
+        price: 189.99,
+        imageUrl: "/imagesProducts/nike-tn-blue.webp", 
+    },
+    {
+        id: "18",
+        name: "Nike-tuned",
+        price: 170.99,
+        imageUrl: "/imagesProducts/nike-tuned.webp", 
+    },
+    {
+        id: "19",
+        name: "Superstar-sean",
+        price: 200.00,
+        imageUrl: "/imagesProducts/superstar-sean.webp", 
+    },
+    {
+        id: "20",
+        name: "Air-retro-4-wet-cement",
+        price: 299.99,
+        imageUrl: "/imagesProducts/retro-4-wet-cement.webp", 
+    },
+    {
+        id: "21",
+        name: "Air-retro-sp-haze",
+        price: 299.99,
+        imageUrl: "/imagesProducts/retro-sp-haze.webp", 
+    },
+    {
+        id: "22",
+        name: "Louis-vuitton-black",
+        price: 999.00,
+        imageUrl: "/imagesProducts/louis-vuitton-black.webp",
+    },
+    {
+        id: "23",
+        name: "Louis-vuitton-cream",
+        price: 999.00,
+        imageUrl: "/imagesProducts/louis-vuitton-cream.webp",
+    },
+    {
+        id: "24",
+        name: "Nike-dior",
+        price: 599.00,
+        imageUrl: "/imagesProducts/nike-dior.webp",
+        soldOut: true,
+    },
+]
