018c68acd54a809718ae7ce502c294040f6e20bf
sebas <sebastianmq027@gmail.com>
2025-03-21 13:55:20 +0100

    Add Astro configuration with React and Tailwind CSS integration

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [react()],
+});
